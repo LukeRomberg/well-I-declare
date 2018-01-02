6fae5ac4c38119a318368ec35c9a8ee9eadc0190
Jake Robbins <jrobbinsd@gmail.com>
2018-01-02 11:57:50 -0700

    click alert is working

--- well-i-react/iDoDeclare/App.js
+++ well-i-react/iDoDeclare/App.js
@@ -9,29 +9,27 @@ import {
   Platform,
   StyleSheet,
   Text,
+	Button,
+	Alert,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component<{}> {
+	_onPressButton() {
+	Alert.alert('You tapped the button!')
+}
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Wecome to I Declare!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+				<View style={styles.buttonContainer}>
+           <Button
+             onPress={this._onPressButton}
+             title="Click for a Southern-Style-Colloquialism" />
+         </View>
       </View>
     );
   }
@@ -42,16 +40,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'purple',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
