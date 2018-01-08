d2d4dbdc0ecd3e535b038fd5a914961c55dd32d2
Luke Romberg <Lukerromberg@gmail.com>
2018-01-08 14:52:46 -0700

    shut up Mike

--- well-i-react/iDoDeclare/App.js
+++ well-i-react/iDoDeclare/App.js
@@ -11,15 +11,16 @@ import {
   Text,
   Button,
   Alert,
-  View
+  View,
+  ScrollView,
 } from 'react-native';
 
 export default class App extends Component < {} > {
   constructor(props) {
     super(props);
     this.state = {
-      phrase: 'test',
-      description: 'test'
+      phrase: 'Howdy!',
+      description: 'Welcome to I Declare! If you need any southern style colloqualisms, feel free to click that button down yander. Don\'t click it too fast, ya hear? If you do, the app will be madder than a wet hen and toss some errors your way. poupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiupoupoiupoiupoiu'
     };
   }
   async _onPressButton() {
@@ -31,17 +32,21 @@ export default class App extends Component < {} > {
   render() {
     return (<View style={styles.container}>
       <Text style={styles.welcome}>
-        Wecome to I Declare!
+        I Declare!
       </Text>
-      <Text>
+      <Text style={styles.saying}>
         {this.state.phrase}
       </Text>
-      <Text>
-        {this.state.description}
+      <ScrollView style={styles.scroll}>
+        <Text style={styles.description}>
+          {this.state.description}
+        </Text>
+      </ScrollView>
+      <View style={styles.button} onPress={this._onPressButton.bind(this)}>
+        <Text style={styles.text} onPress={this._onPressButton.bind(this)}>
+        Let's get it
       </Text>
-      <View style={styles.buttonContainer}>
-        <Button onPress={this._onPressButton.bind(this)} title="Click for a Southern-Style-Colloquialism"/>
-      </View>
+    </View>
     </View>);
   }
 }
@@ -50,12 +55,66 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-
-    backgroundColor: 'purple'
+    backgroundColor: 'grey'
   },
   welcome: {
-    fontSize: 50,
+    textShadowColor: 'white',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 5,
+    fontFamily: 'Snell Roundhand',
+    fontStyle: 'italic',
+    fontSize: 75,
     textAlign: 'center',
-    margin: 10
+    position: 'absolute',
+    top: 20,
+    width: '100%',
+    fontWeight: 'bold',
+  },
+  saying:{
+    textShadowColor: 'black',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 5,
+    fontFamily: 'Didot',
+    fontWeight: 'bold',
+    padding: 10,
+    fontSize: 30,
+    position: 'absolute',
+    top: 100,
+    width: '100%',
+    textAlign: 'center',
+    color: 'white',
+    overflow: 'scroll',
+  },
+  description:{
+    textShadowColor: 'black',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 5,
+    fontFamily: 'Didot',
+    margin: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    color: 'white',
+  },
+  button:{
+    borderWidth: 10,
+    borderColor: 'white',
+    borderRadius: 20,
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: 40,
+  },
+  text:{
+    textShadowColor: 'grey',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 5,
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 25,
+    backgroundColor: 'white',
+  },
+  scroll:{
+    position: 'absolute',
+    top: 165,
+    height: 375,
   }
 });
